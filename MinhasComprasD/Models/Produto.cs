38e4fc13ea2b10ef868c2b09dfd7c720bd9faa7b
Danilo <danilo196@live.com>
2025-03-09 22:44:32 -0300

    SQLITEdatabase helper

--- MinhasComprasD/Models/Produto.cs
+++ MinhasComprasD/Models/Produto.cs
@@ -7,7 +7,7 @@ namespace MinhasComprasD.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Descricao { get; set; }
-        public double quantidade { get; set; }
+        public double Quantidade { get; set; }
         public double Preco { get; set; }
 
     }
