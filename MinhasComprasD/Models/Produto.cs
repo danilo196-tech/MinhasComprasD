8d3f7f77117a10b45f163f27e9f7b6dc6e9e7a56
Danilo <danilo196@live.com>
2025-03-09 22:04:08 -0300

    Add model e troca da tela inicial

--- MinhasComprasD/Models/Produto.cs
+++ MinhasComprasD/Models/Produto.cs
@@ -1,12 +1,14 @@
-﻿kusing System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace MinhasComprasD.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double quantidade { get; set; }
+        public double Preco { get; set; }
+
     }
 }
